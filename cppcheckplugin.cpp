56cc91d3dd63f758f6daff46519ab63f86c3c49a
Kimmo Varis <kimmov@gmail.com>
2010-11-25 20:29:24 +0200

    Add Tools-menu submenu and Check-item.
    Add 'Cppcheck'-submenu to Tools-menu. Into that menu add 'Check'-
    menuitem.

--- cppcheckplugin.cpp
+++ cppcheckplugin.cpp
@@ -1,5 +1,14 @@
 #include <QtPlugin>
+#include <QList>
 #include <QStringList>
+#include <QMenu>
+#include <QAction>
+#include <QKeySequence>
+#include <coreplugin/coreconstants.h>
+#include <coreplugin/actionmanager/actionmanager.h>
+#include <coreplugin/actionmanager/actioncontainer.h>
+#include <coreplugin/actionmanager/command.h>
+#include <coreplugin/icore.h>
 #include "cppcheckplugin.h"
 
 CppcheckPlugin::CppcheckPlugin()
@@ -14,6 +23,19 @@ bool CppcheckPlugin::initialize(const QStringList &args, QString *errMsg)
 {
     Q_UNUSED(args);
     Q_UNUSED(errMsg);
+
+    Core::ActionManager *am = Core::ICore::instance()->actionManager();
+    Core::ActionContainer* submenu = am->createMenu("CppcheckPlugin.CppcheckMenu");
+    submenu->menu()->setTitle("Cppcheck");
+
+    Core::Command* cmd = am->registerAction(new QAction(this),
+            "CppcheckPlugin.Check",
+            QList<int>() << Core::Constants::C_GLOBAL_ID);
+    cmd->action()->setText("Check");
+
+    am->actionContainer(Core::Constants::M_TOOLS)->addMenu(submenu);
+    submenu->addAction(cmd);
+
     return true;
 }
 
